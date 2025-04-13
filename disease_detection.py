fa53072649a3e52e244cfd61f0af1eacad370b4a
gangmanipal <gangmanipal@gmail.com>
2025-04-13 21:25:17 +0530

    Add files via upload

---
+++ disease_detection.py
@@ -0,0 +1,301 @@
+import os                       # for working with files
+import numpy as np              # for numerical computations
+import pandas as pd             # for working with dataframes
+import torch                    # Pytorch module 
+import matplotlib.pyplot as plt # for plotting information on graph and images using tensors
+import torch.nn as nn           # for creating neural networks
+from torch.utils.data import DataLoader # for dataloaders 
+from PIL import Image           # for checking images
+import torch.nn.functional as F # for functions for calculating loss
+import torchvision.transforms as transforms   # for transforming images into tensors 
+from torchvision.utils import make_grid       # for data checking
+from torchvision.datasets import ImageFolder  # for working with classes and images
+from torchsummary import summary              # for getting the summary of our model
+
+# Define data directories
+data_dir = r'C:\Users\Honisha\Desktop\Agrovision\Plant disease dataset'
+train_dir = os.path.join(data_dir, 'train')
+valid_dir = os.path.join(data_dir, 'valid')
+
+diseases = os.listdir(train_dir)
+
+# printing the disease names
+print(diseases)
+print("Total disease classes are: {}".format(len(diseases)))
+
+plants = []
+NumberOfDiseases = 0
+for plant in diseases:
+    if plant.split('___')[0] not in plants:
+        plants.append(plant.split('___')[0])
+    if plant.split('___')[1] != 'healthy':
+        NumberOfDiseases += 1
+
+# unique plants in the dataset
+print(f"Unique Plants are: \n{plants}")
+print("Number of plants: {}".format(len(plants)))
+print("Number of diseases: {}".format(NumberOfDiseases))
+
+# Number of images for each disease
+nums = {}
+for disease in diseases:
+    nums[disease] = len(os.listdir(train_dir + '/' + disease))
+
+# converting to DataFrame
+img_per_class = pd.DataFrame(nums.values(), index=nums.keys(), columns=["no. of images"])
+
+# plotting
+index = [n for n in range(len(diseases))]
+plt.figure(figsize=(20, 5))
+plt.bar(index, [n for n in nums.values()], width=0.3)
+plt.xlabel('Plants/Diseases', fontsize=10)
+plt.ylabel('No of images available', fontsize=10)
+plt.xticks(index, diseases, fontsize=5, rotation=90)
+plt.title('Images per each class of plant disease')
+
+n_train = sum(nums.values())
+print(f"There are {n_train} images for training")
+
+# Load datasets
+train = ImageFolder(train_dir, transform=transforms.ToTensor())
+valid = ImageFolder(valid_dir, transform=transforms.ToTensor()) 
+
+img, label = train[0]
+print(img.shape, label)
+
+# function to show image
+def show_image(image, label):
+    print("Label :" + train.classes[label] + "(" + str(label) + ")")
+    plt.imshow(image.permute(1, 2, 0))
+
+show_image(*train[0])
+
+# Set seed and batch size
+random_seed = 7
+torch.manual_seed(random_seed)
+batch_size = 32
+
+# DataLoaders
+train_dl = DataLoader(train, batch_size, shuffle=True, num_workers=2, pin_memory=True)
+valid_dl = DataLoader(valid, batch_size, num_workers=2, pin_memory=True)
+
+def show_batch(data):
+    for images, labels in data:
+        fig, ax = plt.subplots(figsize=(30, 30))
+        ax.set_xticks([]); ax.set_yticks([])
+        ax.imshow(make_grid(images, nrow=8).permute(1, 2, 0))
+        break
+
+show_batch(train_dl)
+
+# Device functions
+def get_default_device():
+    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def to_device(data, device):
+    if isinstance(data, (list, tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+class DeviceDataLoader():
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+
+    def __iter__(self):
+        for b in self.dl:
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        return len(self.dl)
+
+device = get_default_device()
+train_dl = DeviceDataLoader(train_dl, device)
+valid_dl = DeviceDataLoader(valid_dl, device)
+
+# Residual Block
+class SimpleResidualBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 3, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(3, 3, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.ReLU()
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.relu1(out)
+        out = self.conv2(out)
+        return self.relu2(out) + x
+
+# Accuracy
+
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+# Base class
+class ImageClassificationBase(nn.Module):
+    def training_step(self, batch):
+        images, labels = batch
+        out = self(images)
+        loss = F.cross_entropy(out, labels)
+        return loss
+
+    def validation_step(self, batch):
+        images, labels = batch
+        out = self(images)
+        loss = F.cross_entropy(out, labels)
+        acc = accuracy(out, labels)
+        return {"val_loss": loss.detach(), "val_accuracy": acc}
+
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x["val_loss"] for x in outputs]
+        batch_accuracy = [x["val_accuracy"] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()
+        epoch_accuracy = torch.stack(batch_accuracy).mean()
+        return {"val_loss": epoch_loss, "val_accuracy": epoch_accuracy}
+
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], last_lr: {:.5f}, train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch, result['lrs'][-1], result['train_loss'], result['val_loss'], result['val_accuracy']))
+
+# Conv block
+
+def ConvBlock(in_channels, out_channels, pool=False):
+    layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+              nn.BatchNorm2d(out_channels),
+              nn.ReLU(inplace=True)]
+    if pool:
+        layers.append(nn.MaxPool2d(4))
+    return nn.Sequential(*layers)
+
+# ResNet9
+class ResNet9(ImageClassificationBase):
+    def __init__(self, in_channels, num_diseases):
+        super().__init__()
+        self.conv1 = ConvBlock(in_channels, 64)
+        self.conv2 = ConvBlock(64, 128, pool=True)
+        self.res1 = nn.Sequential(ConvBlock(128, 128), ConvBlock(128, 128))
+        self.conv3 = ConvBlock(128, 256, pool=True)
+        self.conv4 = ConvBlock(256, 512, pool=True)
+        self.res2 = nn.Sequential(ConvBlock(512, 512), ConvBlock(512, 512))
+        self.classifier = nn.Sequential(nn.MaxPool2d(4), nn.Flatten(), nn.Linear(512, num_diseases))
+
+    def forward(self, xb):
+        out = self.conv1(xb)
+        out = self.conv2(out)
+        out = self.res1(out) + out
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.res2(out) + out
+        out = self.classifier(out)
+        return out
+
+model = to_device(ResNet9(3, len(train.classes)), device)
+INPUT_SHAPE = (3, 256, 256)
+print(summary(model.cuda(), (INPUT_SHAPE)))
+
+@torch.no_grad()
+def evaluate(model, val_loader):
+    model.eval()
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+
+def fit_OneCycle(epochs, max_lr, model, train_loader, val_loader, weight_decay=0, grad_clip=None, opt_func=torch.optim.SGD):
+    torch.cuda.empty_cache()
+    history = []
+    optimizer = opt_func(model.parameters(), max_lr, weight_decay=weight_decay)
+    sched = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr, epochs=epochs, steps_per_epoch=len(train_loader))
+
+    for epoch in range(epochs):
+        model.train()
+        train_losses = []
+        lrs = []
+        for batch in train_loader:
+            loss = model.training_step(batch)
+            train_losses.append(loss)
+            loss.backward()
+
+            if grad_clip:
+                nn.utils.clip_grad_value_(model.parameters(), grad_clip)
+
+            optimizer.step()
+            optimizer.zero_grad()
+            lrs.append(get_lr(optimizer))
+            sched.step()
+
+        result = evaluate(model, val_loader)
+        result['train_loss'] = torch.stack(train_losses).mean().item()
+        result['lrs'] = lrs
+        model.epoch_end(epoch, result)
+        history.append(result)
+
+    return history
+
+def plot_accuracies(history):
+    accuracies = [x['val_accuracy'] for x in history]
+    plt.plot(accuracies, '-x')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.title('Accuracy vs. No. of epochs')
+
+def plot_losses(history):
+    train_losses = [x.get('train_loss') for x in history]
+    val_losses = [x['val_loss'] for x in history]
+    plt.plot(train_losses, '-bx')
+    plt.plot(val_losses, '-rx')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.legend(['Training', 'Validation'])
+    plt.title('Loss vs. No. of epochs')
+
+def plot_lrs(history):
+    lrs = np.concatenate([x.get('lrs', []) for x in history])
+    plt.plot(lrs)
+    plt.xlabel('Batch no.')
+    plt.ylabel('Learning rate')
+    plt.title('Learning Rate vs. Batch no.')
+
+def predict_image(img, model):
+    xb = to_device(img.unsqueeze(0), device)
+    yb = model(xb)
+    _, preds = torch.max(yb, dim=1)
+    return train.classes[preds[0].item()]
+
+if __name__ == '__main__':
+    history = [evaluate(model, valid_dl)]
+    epochs = 2
+    max_lr = 0.01
+    grad_clip = 0.1
+    weight_decay = 1e-4
+    opt_func = torch.optim.Adam
+
+    history += fit_OneCycle(epochs, max_lr, model, train_dl, valid_dl, 
+                            grad_clip=grad_clip, 
+                            weight_decay=weight_decay, 
+                            opt_func=opt_func)
+
+    plot_accuracies(history)
+    plot_losses(history)
+    plot_lrs(history)
+
+    test_dir = "../input/new-plant-diseases-dataset/test"
+    test = ImageFolder(test_dir, transform=transforms.ToTensor())
+
+    test_images = sorted(os.listdir(test_dir + '/test'))
+    img, label = test[0]
+    plt.imshow(img.permute(1, 2, 0))
+    print('Label:', test_images[0], ', Predicted:', predict_image(img, model))
+
+    for i, (img, label) in enumerate(test):
+        print('Label:', test_images[i], ', Predicted:', predict_image(img, model))
+
+    PATH = './plant-disease-model.pth'  
+    torch.save(model.state_dict(), PATH)
+    PATH = './plant-disease-model-complete.pth'
+    torch.save(model, PATH)
